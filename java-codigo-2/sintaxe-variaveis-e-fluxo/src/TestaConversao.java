7f4a661c0a19c74fb9635c1c3fdba8491d9119e8
alineslima <alineslima@outlook.com>
2020-07-28 19:42:32 -0300

    curso de java 01 da Alura

---
+++ java-codigo-2/sintaxe-variaveis-e-fluxo/src/TestaConversao.java
@@ -0,0 +1,17 @@
+
+public class TestaConversao {
+	
+	public static void main(String[] args) {
+		double salario=1270.50;
+		int valor = (int) salario;
+		System.out.println(valor);
+		
+		float ponto1 = 3.14f;
+		System.out.println(ponto1);
+		
+		float ponto2 = (float) 3.14;
+		System.out.println(ponto2);
+		
+	}
+
+}
